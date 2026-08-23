dabc1fb6946554bc2b9287e239a1b0eb9413e2e7
Sobur Ahmed <soburwebdevs@gmail.com>
2026-08-23 21:00:28 +0600

    Add nested cart item endpoints with quantity merging, update, and delete support

--- store/urls.py
+++ store/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_nested import routers
 from store import views
 
 router = DefaultRouter()
 router.register('categories', views.CategoryViewSet, basename='category')
 router.register('products', views.ProductViewSet, basename='product')
+router.register('carts', views.CartViewSet, basename='cart')
+
+carts_router = routers.NestedDefaultRouter(router, 'carts', lookup='cart')
+carts_router.register('items', views.CartItemViewSet, basename='cart-items')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('', include(carts_router.urls)),
 ]
--- store/views.py
+++ store/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, GenericViewSet
+from rest_framework.mixins import CreateModelMixin, DestroyModelMixin, RetrieveModelMixin, UpdateModelMixin
 from store import models, serializers
 
 
@@ -13,5 +14,23 @@ class ProductViewSet(ModelViewSet):
 class CategoryViewSet(ModelViewSet):
     queryset = models.Category.objects.all()
     serializer_class = serializers.CategorySerializer
+
+
+class CartViewSet(CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, GenericViewSet):
+    queryset = models.Cart.objects.prefetch_related('items__product').all()
+    serializer_class = serializers.CartSerializer
+
+
+class CartItemViewSet(CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, UpdateModelMixin, GenericViewSet):
+    def get_serializer_class(self):
+        if self.request.method == 'POST':
+            return serializers.AddCartItemSerializer
+        return serializers.CartItemSerializer
+    
+    def get_serializer_context(self):
+        return {'cart_id': self.kwargs['cart_pk']}
+    
+    def get_queryset(self):
+        return models.CartItem.objects.filter(cart_id=self.kwargs['cart_pk'])
 
 

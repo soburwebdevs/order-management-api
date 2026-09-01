21755ed95d8b2ebb2840da3d746d0ca32bdcfef3
Sobur Ahmed <soburwebdevs@gmail.com>
2026-09-01 19:21:11 +0600

    Migrate database to SQLite to PostgreSQL

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 from datetime import timedelta
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t@ng9t=d0&-n%(yuvzm%f16@a7r0%$4iyy1-5(186#6*++7ax0'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
 
 
 # Application definition
@@ -79,8 +80,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME', default='placeholder'),
+        'USER': config('DB_USER', default='placeholder'),
+        'PASSWORD': config('DB_PASSWORD', default='placeholder'),
+        'HOST': config('DB_HOST', default='placeholder'),
+        'PORT': config('DB_PORT', default='5432'),
     }
 }
 

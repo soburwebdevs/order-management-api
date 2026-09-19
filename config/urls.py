b88bc287ac7ac40c4d67234f38db873e57105d47
Sobur Ahmed <soburwebdevs@gmail.com>
2026-09-19 18:10:24 +0600

    setup api_root to show all endpoints in root url

--- config/urls.py
+++ config/urls.py
@@ -17,9 +17,29 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'categories': reverse('category-list', request=request, format=format),
+        'products': reverse('product-list', request=request, format=format),
+        'carts': reverse('cart-list', request=request, format=format),
+        'orders': reverse('order-list', request=request, format=format),
+        'register': reverse('register', request=request, format=format),
+        'login': reverse('login', request=request, format=format),
+        'logout': reverse('logout', request=request, format=format),
+        'token_refresh': reverse('token_refresh', request=request, format=format),
+        'note': 'To view/edit a specific product, go to /store/products/<id>/. To manage a specific cart, go to /store/carts/<uuid>/ and /store/carts/<uuid>/items/.',
+    })
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', api_root, name='api-root'),
     path('api-auth/', include('rest_framework.urls')),
     path('store/', include('store.urls')),
     path('account/', include('accounts.urls')),
